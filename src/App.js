1c77f4d180ed0e3aa0095500fc3ccd68f4d5a584
Akashdeep <aakashdeepshrivastvagopaani@gmail.com>
2023-06-30 23:38:32 +0530

    side drawer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import BottomNav from './components/BottomNav';
 import Header from './components/Navbar';
 import "tailwindcss/tailwind.css";
 import Drawer from './components/drawerNav';
+import AllGames from './screens/AllGames';
 function App() {
   return (
     <main>
+      <AllGames/>
       <BottomNav/>
     </main>
 
--- src/components/drawerNav.js
+++ src/components/drawerNav.js
@@ -12,60 +12,60 @@ const Drawer = () => {
   return (
     <>
         <div class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" onClick={openDrawer}>
-            <svg class="w-6 h-6 text-red-500 dark:text-red-400 group-hover:text-gray-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <svg class="w-6 h-6 text-yellow-500 dark:text-yellow-400 group-hover:text-gray-600 dark:group-hover:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path>
             </svg>
-            <span class="text-sm text-red-500 dark:text-red-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Menu</span>
+            <span class="text-sm text-yellow-500 dark:text-yellow-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Menu</span>
         </div>
 
       <div
-        className={`bg-transparent fixed top-0 right-0 h-screen w-64 z-50 ${
+        className={`bg-transparent fixed top-0 right-0 h-[48.8rem] w-64 z-50 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         } ease-in-out duration-500`}
       >
-        <div className="flex items-center justify-between m-5 pb-5 border-b border-b-gray-300">
-          <span className="text-lg font-bold text-cyan-500 cursor-pointer">
-            Tailwind.
+        <div className="flex items-center justify-around m-2 pb-3 pt-2 border-b border-b-gray-300">
+          <span onClick={() => setIsOpen(false)} className="text-2xl italic font-bold text-yellow-500 cursor-pointer">
+            LETSLUDO
           </span>
 
           <span onClick={() => setIsOpen(false)}>
-            <IoMdClose className="w-6 h-6 text-gray-500" />
+            <IoMdClose className="w-10 h-10 bg-yellow-400 rounded-3xl text-gray-800" />
           </span>
         </div>
 
         <div className="m-5">
           <ul className="flex flex-col items-end gap-7  transition-all ease-in duration-500">
-            <li className="">
+            <li className="border-b border-b-gray-300">
               <a
                 href="/"
-                className="text-lg text-slate-700 transition-all ease-in-out duration-300"
+                className="text-xl text-slate-700 font-semibold transition-all ease-in-out duration-300"
               >
                 Games History
               </a>
             </li>
 
-            <li className="">
+            <li className="border-b border-b-gray-300">
               <a
                 href="/"
-                className="text-lg text-slate-700  transition-all ease-in-out duration-300"
+                className="text-xl text-slate-700 font-semibold transition-all ease-in-out duration-300"
               >
                 Transactions
               </a>
             </li>
 
-            <li className="">
+            <li className="border-b border-b-gray-300">
               <a
                 href="/"
-                className="text-lg text-slate-700  transition-all ease-in-out duration-300"
+                className="text-xl text-slate-700 font-semibold transition-all ease-in-out duration-300"
               >
                 Notifications
               </a>
             </li>
 
-            <li className="">
+            <li className="border-b border-b-gray-300">
               <a
                 href="/"
-                className="text-lg text-slate-700 transition-all ease-in-out duration-300"
+                className="text-xl text-slate-700 font-semibold transition-all ease-in-out duration-300"
               >
                Refer and Earn
               </a>
